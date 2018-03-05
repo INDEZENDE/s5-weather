5cae3f66ea44baa834a3ba1391c8e1aec62f64cf
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-04 23:04:09 -0600

    Utilizando API darksky

---
+++ README.md
@@ -0,0 +1,13 @@
+# WEATHER
+
+*Utilizar la API Dark sky weather, la cual ofrece datos oportunos y actualizados de las predicciones del clima de acuerdo a tu localización*
+
+#### 1. Muestra los datos de:
+* Temperature
+* Humidity
+* UV Index
+* pressure
+
+#### 2. Muestra los datos de los datos de los próximos 7 días obtenidos desde el API.
+
+#### 3 La vista es mobile frendly.
---
+++ js/app.js
@@ -0,0 +1,33 @@
+var proxy = 'https://cors-anywhere.herokuapp.com/';//por el error proxy is not defined
+var urlDs = "https://api.darksky.net/forecast/18ec6aa827606a859cfcf55f80be703b/19.2464696,-99.1013497999999/?units=ca&?lang=es";
+//https://api.darksky.net/forecast/18ec6aa827606a859cfcf55f80be703b/37.8267,-122.4233
+$.ajax({
+  url: proxy + urlDs,
+}).done(handleResponse).then(paintWeekWeather).fail(handleFailure)
+
+function handleResponse(data) {
+  $temperature=document.querySelector("#temperature").textContent=(data.currently.temperature+"°");
+  $wind=document.querySelector("#wind").textContent=(data.currently.windSpeed+"m/s");
+  $humidity=document.querySelector("#humidity").textContent=(data.currently.humidity+"%");
+  $uvIndex=document.querySelector("#uvIndex").textContent=(data.currently.uvIndex);
+  $pressure=document.querySelector("#pressure").textContent=(data.currently.pressure+"hPa");
+  $('#card2').hide();
+  $('#btn-week').click(function(data){
+    $('#card2').show();
+  });
+}
+
+function  handleFailure(){
+  console.log("falla");
+}
+
+function paintWeekWeather  (data) {
+
+     $('#monday').text((data['daily']['data'][0]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][0]['temperatureMax']) + '°');
+     $('#tuesday').text((data['daily']['data'][1]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][1]['temperatureMax']) + '°');
+     $('#wednesday').text((data['daily']['data'][2]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][2]['temperatureMax']) + '°');
+     $('#thursday').text((data['daily']['data'][3]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][3]['temperatureMax']) + '°');
+     $('#friday').text((data['daily']['data'][4]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][4]['temperatureMax']) + '°');
+     $('#saturday').text((data['daily']['data'][5]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][5]['temperatureMax']) + '°');
+     $('#sunday').text((data['daily']['data'][6]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][6]['temperatureMax']) + '°');
+}
