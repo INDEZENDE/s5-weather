98631989a03e36a60a38b3715dba8e9683c23da1
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-07 11:48:53 -0600

    Mejorando sintaxis

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 var proxy = 'https://cors-anywhere.herokuapp.com/';//por el error proxy is not defined
 var urlDs = "https://api.darksky.net/forecast/18ec6aa827606a859cfcf55f80be703b/19.2464696,-99.1013497999999/?units=ca&?lang=es";
 //https://api.darksky.net/forecast/18ec6aa827606a859cfcf55f80be703b/37.8267,-122.4233
+
+
+
 $.ajax({
   url: proxy + urlDs,
-}).done(handleResponse).then(paintWeekWeather).fail(handleFailure)
+}).done(handleResponse).done(paintWeekWeather).fail(handleFailure)
 
 function handleResponse(data) {
+
   $temperature=document.querySelector("#temperature").textContent=(data.currently.temperature+"°");
   $wind=document.querySelector("#wind").textContent=(data.currently.windSpeed+"m/s");
   $humidity=document.querySelector("#humidity").textContent=(data.currently.humidity+"%");
@@ -17,12 +21,9 @@ function handleResponse(data) {
   });
 }
 
-function  handleFailure(){
-  console.log("falla");
-}
+
 
 function paintWeekWeather  (data) {
-
      $('#monday').text((data['daily']['data'][0]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][0]['temperatureMax']) + '°');
      $('#tuesday').text((data['daily']['data'][1]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][1]['temperatureMax']) + '°');
      $('#wednesday').text((data['daily']['data'][2]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][2]['temperatureMax']) + '°');
@@ -30,4 +31,8 @@ function paintWeekWeather  (data) {
      $('#friday').text((data['daily']['data'][4]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][4]['temperatureMax']) + '°');
      $('#saturday').text((data['daily']['data'][5]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][5]['temperatureMax']) + '°');
      $('#sunday').text((data['daily']['data'][6]['temperatureMin']) + '°' + ' - ' + (data['daily']['data'][6]['temperatureMax']) + '°');
+}
+
+function  handleFailure(){
+  console.log("falla");
 }
